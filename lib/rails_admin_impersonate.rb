d2bf77dd23df7c66d4100590f0926dcd4080d0de
Noah Pryor <noah@noahpryor.com>
2015-02-05 00:54:59 -0500

    Update rails_admin_impersonate.rb

--- lib/rails_admin_impersonate.rb
+++ lib/rails_admin_impersonate.rb
@@ -34,6 +34,36 @@ module RailsAdmin
 
         RailsAdmin::Config::Actions.register(self)
       end
+      class BrowserStack < RailsAdmin::Config::Actions::Base
+        register_instance_option :visible? do
+          ('Admin' != bindings[:abstract_model].model_name) &&
+            authorized? && bindings[:object].respond_to?(:devise_modules)
+        end
+
+        register_instance_option :member? do
+          true
+        end
+
+        register_instance_option :pjax? do
+          false
+        end
+
+        register_instance_option :http_methods do
+          [:get]
+        end
+
+        register_instance_option :link_icon do
+          'icon-globe'
+        end
+
+        register_instance_option :controller do
+          Proc.new do
+            redirect_to @object.browserstack_url
+          end
+        end
+
+        RailsAdmin::Config::Actions.register(self)
+      end
     end
   end
 end
